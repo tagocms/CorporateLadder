c5973a4350353975fa149130bd6998db80b695d4
Tiago Camargo Maciel dos Santos <tagocms@gmail.com>
2025-07-30 09:06:24 -0300

    Initial Commit

---
+++ CorporateLadder/CorporateLadderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CorporateLadderApp.swift
+//  CorporateLadder
+//
+//  Created by Tiago Camargo Maciel dos Santos on 30/07/25.
+//
+
+import SwiftUI
+
+@main
+struct CorporateLadderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
